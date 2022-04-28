98d0cd60c6c64e7362baa64666155b7d4c0ec1e3
Vlatko Marchan <alu0101321141@ull.edu.es>
2022-04-28 08:43:45 +0000

    Modificamos la ruta del ejercicio 1 para poder ser ejecutada correctamente

--- src/Ejercicio1/ejercicio1.ts
+++ src/Ejercicio1/ejercicio1.ts
@@ -4,14 +4,14 @@ if (process.argv.length !== 3) {
   console.log('Please, specify a file');
 } else {
   const filename = process.argv[2];
-
-  access(filename, constants.F_OK, (err) => {
+  const routeFile = `./src/Ejercicio1/${filename}`;
+  access(routeFile, constants.F_OK, (err) => {
     if (err) {
       console.log(`File ${filename} does not exist`);
     } else {
       console.log(`Starting to watch file ${filename}`);
 
-      const watcher = watch(process.argv[2]);
+      const watcher = watch(routeFile);
 
       watcher.on('change', () => {
         console.log(`File ${filename} has been modified somehow`);
